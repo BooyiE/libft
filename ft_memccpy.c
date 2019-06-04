72b4cfc676a809b2cf398e0d1790c531d33815c5
Boile PHOFUYAGAE <bphofuya@c4r5s5.wethinkcode.co.za>
2019-06-04 16:16:54 +0200

    ft_memccpy.c

---
+++ ft_memccpy.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memccpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bphofuya <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/04 14:28:38 by bphofuya          #+#    #+#             */
+/*   Updated: 2019/06/04 16:16:20 by bphofuya         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
+{
+	size_t			i;
+	char			*b;
+	char			*s;
+	unsigned char	m;
+
+	b = (char *)dst;
+	s = (char *)src;
+	m = (unsigned char)c;
+	i = 0;
+	while (i < n)
+	{
+		b[i] = s[i];
+		i++;
+		if (s[i] == c)
+		{
+			b[i] = s[i];
+			return (&b[i + 1]);
+		}
+	}
+	return (NULL);
+}
