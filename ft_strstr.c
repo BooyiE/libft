aa6405d575e9041694966489942a3ba1c8b2957d
Boile PHOFUYAGAE <bphofuya@c4r4s1.wethinkcode.co.za>
2019-06-06 16:06:18 +0200

    ft_strstr.c

---
+++ ft_strstr.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bphofuya <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/06 15:36:20 by bphofuya          #+#    #+#             */
+/*   Updated: 2019/06/06 16:05:57 by bphofuya         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strstr(const char *haystack, const char *needle)
+{
+   	unsigned int i;
+    unsigned int j;
+    i = 0;
+    if (haystack[i] == '\0' && needle[i] == '\0')
+        return ((char *)haystack);
+    while (haystack[i] != '\0')
+    {
+        j = 0;
+        while (needle[j] != '\0' && haystack[i + j] == needle[j])
+            j++;
+        if (needle[j] == '\0')
+            return ((char *)haystack + i);
+        i++;
+    }
+    return (NULL);
+}
