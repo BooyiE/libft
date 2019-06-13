254ec01e4fdde7b5b7352151c8b1115466222a2b
Boile PHOFUYAGAE <bphofuya@c4r14s7.wethinkcode.co.za>
2019-06-13 11:17:59 +0200

    ft_atoi.c

---
+++ ft_atoi.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bphofuya <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/07 14:08:53 by bphofuya          #+#    #+#             */
+/*   Updated: 2019/06/07 17:21:56 by bphofuya         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_atoi(const char *str)
+{
+	int i;
+	int neg;
+	int result;
+
+	i = 0;
+	neg = 1;
+	result = 0;
+	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' || str[i] == '\v')
+		str++;
+	while (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+		{
+			neg = neg * -1;
+		}
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = result * 10 + (str[i] - 48);
+		i++;
+	}
+	return (neg * result);
+}
