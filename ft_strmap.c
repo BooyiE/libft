43004d97bc4998b59d636cc2d5833f05b11b0ed3
Boile PHOFUYAGAE <bphofuya@c4r14s7.wethinkcode.co.za>
2019-06-13 12:52:04 +0200

    ft_strmap.c

---
+++ ft_strmap.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bphofuya <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/13 11:43:39 by bphofuya          #+#    #+#             */
+/*   Updated: 2019/06/13 12:51:36 by bphofuya         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmap(char const *s, char (*f)(char))
+{
+	unsigned int	i;
+	char			*str;
+
+	i = 0;
+	if (s != 0)
+	{
+		str = (char *)(malloc(sizeof(char) * (ft_strlen(s) + 1)));
+		if (str == NULL)
+			return (NULL);
+		while (s[i] != 0)
+		{
+			str[i] = f(s[i]);
+			i++;
+		}
+		str[i] = '\0';
+		return (str);
+	}
+	return (NULL);
+}
