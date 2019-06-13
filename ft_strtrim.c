69fa635dbbbe1739204532070dfd15b00a4c8f67
Boile PHOFUYAGAE <bphofuya@c4r14s7.wethinkcode.co.za>
2019-06-13 16:30:02 +0200

    ft_strtrim.c

---
+++ ft_strtrim.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bphofuya <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/10 15:40:09 by bphofuya          #+#    #+#             */
+/*   Updated: 2019/06/13 16:29:41 by bphofuya         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strtrim(char const *s)
+{
+	char const *s_end;
+
+	if (s == NULL)
+		return (NULL);
+	while (*s == ' ' || *s == '\t' || *s == '\n')
+		s++;
+	if (*s == '\0')
+		return (ft_strnew(0));
+	s_end = s + ft_strlen(s) - 1;
+	while (*s_end == ' ' || *s_end == '\t' || *s_end == '\n')
+		s_end--;
+	return (ft_strsub(s, 0, s_end - s + 1));
+}
