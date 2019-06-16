ce3c326f6d4391b3732d22886f430fd42fab3394
Boile PHOFUYAGAE <bphofuya@c4r14s7.wethinkcode.co.za>
2019-06-16 17:03:17 +0200

    ft_strnstr.c

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,34 +6,31 @@
 /*   By: bphofuya <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/06 16:06:53 by bphofuya          #+#    #+#             */
-/*   Updated: 2019/06/06 16:31:48 by bphofuya         ###   ########.fr       */
+/*   Updated: 2019/06/16 17:02:56 by bphofuya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char    *ft_strnstr(const char *h, const char *n,size_t len)
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-   size_t i;
-   size_t m;
+	size_t i;
+	size_t j;
 
-   i = 0;
-   if (n[0] == '\0')
-   {
-       return ((char *)h);
-   }
-   while (h[i] && (i + strlen(n) <= len))
-   {
-       m = 0;
-       while (h[i + m] == n[m])
-       {
-           if (m == strlen(n) - 1)
-           {
-               return (NULL);
-           }
-           m++;
-       }
-       i++;
-   }
-   return 0;
+	i = 0;
+	j = 0;
+	if (needle[j] == '\0')
+		return ((char *)haystack);
+	while (haystack[i] != '\0' && i < len)
+	{
+		while ((haystack[i] == needle[j] || needle[j] == '\0') && i < len)
+		{
+			i++;
+			j++;
+			if (needle[j] == '\0')
+				return ((char *)&haystack[i - j]);
+		}
+		i++;
+	}
+	return (NULL);
 }
