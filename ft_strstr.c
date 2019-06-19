11f32d29aced86bd84da1d3505311289d1c4be30
Boile PHOFUYAGAE <bphofuya@c4r14s7.wethinkcode.co.za>
2019-06-19 13:03:22 +0200

    ft_strstr.c

--- ft_strstr.c
+++ ft_strstr.c
@@ -6,7 +6,7 @@
 /*   By: bphofuya <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/06 15:36:20 by bphofuya          #+#    #+#             */
-/*   Updated: 2019/06/06 16:05:57 by bphofuya         ###   ########.fr       */
+/*   Updated: 2019/06/19 13:03:06 by bphofuya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,20 @@
 
 char	*ft_strstr(const char *haystack, const char *needle)
 {
-   	unsigned int i;
-    unsigned int j;
-    i = 0;
-    if (haystack[i] == '\0' && needle[i] == '\0')
-        return ((char *)haystack);
-    while (haystack[i] != '\0')
-    {
-        j = 0;
-        while (needle[j] != '\0' && haystack[i + j] == needle[j])
-            j++;
-        if (needle[j] == '\0')
-            return ((char *)haystack + i);
-        i++;
-    }
-    return (NULL);
+	unsigned int i;
+	unsigned int j;
+
+	i = 0;
+	if (haystack[i] == '\0' && needle[i] == '\0')
+		return ((char *)haystack);
+	while (haystack[i] != '\0')
+	{
+		j = 0;
+		while (needle[j] != '\0' && haystack[i + j] == needle[j])
+			j++;
+		if (needle[j] == '\0')
+			return ((char *)haystack + i);
+		i++;
+	}
+	return (NULL);
 }
