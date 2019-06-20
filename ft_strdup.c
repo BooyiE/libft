4364248fd60293abe696e2b876c1373d2f6ea74c
Boile PHOFUYAGAE <bphofuya@c4r14s7.wethinkcode.co.za>
2019-06-20 17:33:52 +0200

    ft_strdup.c

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: bphofuya <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/06 11:58:54 by bphofuya          #+#    #+#             */
-/*   Updated: 2019/06/06 12:51:49 by bphofuya         ###   ########.fr       */
+/*   Updated: 2019/06/20 17:33:37 by bphofuya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,18 @@
 
 char	*ft_strdup(const char *s1)
 {
-    char    *copy;
-    int        i;
+	char	*copy;
+	int		i;
 
-    i =-1;
-    if (!s1 || !(copy = (char *)malloc(sizeof(char)* ft_strlen(s1) + 1)))
-    {
-        return (NULL);
-    }
-    while (s1[++i])
-    {
-        copy[i] = s1[i];
-    }
-    copy[i] = '\0';
-    return (copy);
-} 
+	i = -1;
+	if (!s1 || !(copy = (char *)malloc(sizeof(char) * ft_strlen(s1) + 1)))
+	{
+		return (NULL);
+	}
+	while (s1[++i])
+	{
+		copy[i] = s1[i];
+	}
+	copy[i] = '\0';
+	return (copy);
+}
