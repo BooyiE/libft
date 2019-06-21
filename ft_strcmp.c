5188b5b9799751be4eac74f4a2e4da19f980467e
Boile PHOFUYAGAE <bphofuya@c4r14s7.wethinkcode.co.za>
2019-06-22 01:23:21 +0200

    ft_strcmp.c

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -6,13 +6,11 @@
 /*   By: bphofuya <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 14:59:05 by bphofuya          #+#    #+#             */
-/*   Updated: 2019/06/14 16:52:52 by bphofuya         ###   ########.fr       */
+/*   Updated: 2019/06/22 01:22:52 by bphofuya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-#include <string.h>
-#include <stdio.h>
+#include "libft.h"
 
 int	ft_strcmp(const char *s1, const char *s2)
 {
@@ -21,5 +19,5 @@ int	ft_strcmp(const char *s1, const char *s2)
 	i = 0;
 	while ((s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0'))
 		i++;
-	return (s1[i] - s2[i]);
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
