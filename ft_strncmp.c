bec0f03ac8a328309c087d12ea94574d43b07b4b
Boile PHOFUYAGAE <bphofuya@c4r14s7.wethinkcode.co.za>
2019-06-22 06:58:42 +0200

    ft_strncmp.c

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: bphofuya <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/07 12:38:37 by bphofuya          #+#    #+#             */
-/*   Updated: 2019/06/22 01:26:18 by bphofuya         ###   ########.fr       */
+/*   Updated: 2019/06/22 03:23:28 by bphofuya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,16 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t	i;
 
 	i = 0;
-	while (((s1[i] == s2[i]) && s1[i] != '\0') && (s2[i] != '\0' && i < n))
+	if (n == 0)
+		return (0);
+	while ((s1[i] == s2[i] && s1[i] != '\0') && (s2[i] != '\0' && i < n - 1))
 	{
 		i++;
 	}
-	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+	if ((unsigned char)s1[i] - (unsigned char)s2[i] > 0)
+		return (1);
+	else if ((unsigned char)s1[i] - (unsigned char)s2[i] < 0)
+		return (-1);
+	else
+		return (0);
 }
