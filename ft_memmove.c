88bcdb7a84afa035b9a4de72cadc78f7f07fc155
Boile PHOFUYAGAE <bphofuya@c4r14s7.wethinkcode.co.za>
2019-07-02 13:59:55 +0200

    ft_memmove.c

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: bphofuya <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/05 12:05:30 by bphofuya          #+#    #+#             */
-/*   Updated: 2019/07/02 13:39:26 by bphofuya         ###   ########.fr       */
+/*   Updated: 2019/07/02 13:59:30 by bphofuya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	char    *s;
-	char    *d;
+	char	*s;
+	char	*d;
 
 	s = (char *)src;
 	d = (char *)dst;
