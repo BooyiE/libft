8431cf1797401f3b874c3359e5f4be3b28fb729f
Boile PHOFUYAGAE <bphofuya@c4r14s7.wethinkcode.co.za>
2019-07-02 12:19:00 +0200

    ft_strrchr.c

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: bphofuya <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/06 15:05:54 by bphofuya          #+#    #+#             */
-/*   Updated: 2019/06/06 15:27:34 by bphofuya         ###   ########.fr       */
+/*   Updated: 2019/07/02 12:18:46 by bphofuya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,14 @@
 
 char	*ft_strrchr(const char *s, int c)
 {
+	int i;
+
+	i = 0;
 	if (ft_strchr(s, c) == NULL)
 		return (NULL);
-	while (*s)
-		s++;
-	while (*s != c)
-		s--;
-	return ((char *)s);
+	while (s[i])
+		i++;
+	while (s[i] != c)
+		i--;
+	return ((char *)&s[i]);
 }
