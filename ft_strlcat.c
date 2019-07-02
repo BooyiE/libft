05dadb66bf81c1b3d750f5cfb94ce66d5c396420
Boile PHOFUYAGAE <bphofuya@c4r14s7.wethinkcode.co.za>
2019-07-02 13:29:14 +0200

    ft_strlcat.c

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: bphofuya <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/31 13:01:04 by bphofuya          #+#    #+#             */
-/*   Updated: 2019/07/02 12:26:19 by bphofuya         ###   ########.fr       */
+/*   Updated: 2019/07/02 13:23:37 by bphofuya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,15 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 	size_t	len;
 
 	len = 0;
-	while (dst[len] && len < size)
+	while (dst[len] && len < dstsize)
 		len += 1;
 	i = len;
-	while (src[len - i] && len + 1 < size)
+	while (src[len - i] && len + 1 < dstsize)
 	{
 		dst[len] = src[len - i];
 		len += 1;
 	}
-	if (i < size)
+	if (i < dstsize)
 		dst[len] = '\0';
 	return (i + ft_strlen(src));
 }
